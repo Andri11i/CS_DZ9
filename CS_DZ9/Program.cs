4819b5b826f2d88c17384ac486bf97de5ec59f14
zotov.andrey.2008@gmail.com <zotov.andrey.2008@gmail.com>
2024-09-01 11:56:41 +0300

    .

--- CS_DZ9/Program.cs
+++ CS_DZ9/Program.cs
@@ -1,27 +1,22 @@
 ﻿using CS_DZ9;
 using System.Reflection;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Type type = typeof(SampleClass);
-        MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+Type type = typeof(SampleClass);
+MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
 
-        foreach (var method in methods)
+foreach (var method in methods)
+{
+    var attributes = method.GetCustomAttributes(typeof(DocumentationAttribute), false);
+
+    foreach (DocumentationAttribute attr in attributes)
+    {
+        Console.WriteLine($"Method: {method.Name}");
+        Console.WriteLine($"  Author: {attr.Author}");
+        Console.WriteLine($"  Version: {attr.Version}");
+        if (!string.IsNullOrEmpty(attr.Desc))
         {
-            var attributes = method.GetCustomAttributes(typeof(DocumentationAttribute), false);
-            foreach (DocumentationAttribute attr in attributes)
-            {
-                Console.WriteLine($"Method: {method.Name}");
-                Console.WriteLine($"....Author: {attr.Author}");
-                Console.WriteLine($"....Version: {attr.Version}");
-                if (!string.IsNullOrEmpty(attr.Desc))
-                {
-                    Console.WriteLine($"......Description: {attr.Desc}");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine($"  Description: {attr.Desc}");
         }
+        Console.WriteLine();
     }
 }
